42229dfcb2e37b5bbfc38756273de413b52b42f0
Gelpa99 <759406@unizar.es>
2022-04-06 12:40:04 +0200

    static board

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,14 @@ import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import { HomeComponent } from "./home/home.component";
+import { TableroComponent } from "./tablerov1/tablero.component";
+
 
 const appRoutes = [
     { path: '', component: AppComponent, pathMatch: "full"},
     { path: 'register', component: RegisterComponent, pathMatch: "full"},
     { path: 'login', component: LoginComponent, pathMatch: "full"},
-    { path: 'home', component: HomeComponent, pathMatch: "full"}
+    { path: 'home', component: HomeComponent, pathMatch: "full"},
+    { path: 'tablero', component: TableroComponent, pathMatch: "full"}
 ];
 export const routing = RouterModule.forRoot(appRoutes);
