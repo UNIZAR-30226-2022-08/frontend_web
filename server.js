2faf26f7fc218d8d3c7a0eb360dc5d61803d943a
Gelpa99 <759406@unizar.es>
2022-04-16 14:17:39 +0200

    heroku testsv8

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const app = express();
-
-// Serve static files
 app.use(express.static(__dirname + '/dist/frontend_web'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/frontend_web/index.html'));
-});
-
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/frontend_web/index.html'));});
+app.listen(process.env.PORT || 8080);
