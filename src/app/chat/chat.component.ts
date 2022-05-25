9e848a9aa794cc0035bed3f0dbd3ea3643f3e3a1
JuanchoZGZ17 <756640@unizar.es>
2022-05-25 19:31:24 +0200

    Checkbug corregido

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -20,6 +20,24 @@ export class ChatComponent {
   }
   
   ngOnInit() {
+    axios
+    .get('https://queenchess-backend.herokuapp.com/account/checkSession', {
+    })
+    .then((res) => {
+      if (res.status === 200) {
+        localStorage.setItem("user", res.data.response.username);
+        localStorage.setItem("email", res.data.response.email);
+        console.log("Response session data: " + res.data);
+        console.log("Storing username: " + localStorage.getItem("user"));
+      } else if (res.status === 400) {
+        localStorage.clear();
+        console.log("check session error: " + res.status);
+        this.router.navigateByUrl('/home');
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    })
     this.chatService
       .getMessages()
       .subscribe((message: string) => {
